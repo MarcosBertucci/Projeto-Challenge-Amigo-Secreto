7cbb9fe39a9386bdbaaeda13e82f373a1ec16688
Marcos Bertucci <m.vitti.bertucci@hotmail.com>
2025-02-08 13:49:37 -0300

    Alterando alert

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,2 +1,8 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 alert('bem vindo')
+let numero = 10;
+if(numero >= 5){
+    alert('Ei, sai fora mano!');
+}else{
+    alert('Haha se fudeu!');
+}
