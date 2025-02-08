2c66d81a97575ffce4dd427d6bef33d8f39e6da7
Marcos Bertucci <m.vitti.bertucci@hotmail.com>
2025-02-08 14:25:29 -0300

    teste

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,10 +1,7 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
-alert('bem vindo')
+
 let numero = 10;
-if(numero >= 5){
-    alert('Ei, sai fora mano!');
-}else{
-    alert('Haha se fudeu!');
-}
-let nome = prompt('Qual seu nome?');
-alet(`Olá, ${name}!`);
+
+
+let numeroAleatorio = parseInt(Math.random() * 100 + 1);
+console.log(numeroAleatorio)
